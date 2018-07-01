6b149363db195ad612bf22fa75af05f8b0aabc48
rodrigo xavier <rodrigomatiasxavier@gmail.com>
2018-07-01 22:46:11 +0000

    Preparando para fazer o merge com a master

--- src/Board.cpp
+++ src/Board.cpp
@@ -2,53 +2,15 @@
 
 Board::Board(void)
 {
-    initBoard();
-    // king = new King();
-    // knight = new Knight();
-    // pawn = new Pawn();
-    // piece = new Piece();
-    // queen = new Queen();
-    // rook = new Rook();
-
-    // Alocar as peças para o jogo neste construtor
 }
 
 Board::~Board()
 {
-    destroyBoard();
-    // delete king;
-    // delete knight;
-    // delete pawn;
-    // delete piece;
-    // delete queen;
-    // delete rook;
-    // Chamar destrutores das peças neste destrutor
-}
-
-void Board::initBoard(void)
-{
-    board[8][8] =
-    't', 'c', 'b', 'k', 'q', 'b', 'c', 't',
-    'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p',
-    '0', '0', '0', '0', '0', '0', '0', '0',
-    '0', '0', '0', '0', '0', '0', '0', '0',
-    '0', '0', '0', '0', '0', '0', '0', '0',
-    '0', '0', '0', '0', '0', '0', '0', '0',
-    'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p',
-    't', 'c', 'b', 'q', 'k', 'b', 'c', 't';
-}
-
-void Board::destroyBoard(void)
-{
-    board[8][8] =
-    '0', '0', '0', '0', '0', '0', '0', '0',
-    '0', '0', '0', '0', '0', '0', '0', '0',
-    '0', '0', '0', '0', '0', '0', '0', '0',
-    '0', '0', '0', '0', '0', '0', '0', '0',
-    '0', '0', '0', '0', '0', '0', '0', '0',
-    '0', '0', '0', '0', '0', '0', '0', '0',
-    '0', '0', '0', '0', '0', '0', '0', '0',
-    '0', '0', '0', '0', '0', '0', '0', '0';
+    int i, j;
+    
+    for(i = 0; i < 8; i++)
+        for(j = 0; j < 8; j++)
+            board[i][j] = '0';
 }
 
 char Board::returnPiece(int x, int y)
