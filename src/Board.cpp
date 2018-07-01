5038a51ec5efedc412043f52198890e79f2fb4fd
rodrigo xavier <rodrigomatiasxavier@gmail.com>
2018-07-01 21:29:37 +0000

    Modificando a estrutura do tabuleiro

--- src/Board.cpp
+++ src/Board.cpp
@@ -3,20 +3,32 @@
 Board::Board(void)
 {
     initBoard();
+    // king = new King();
+    // knight = new Knight();
+    // pawn = new Pawn();
+    // piece = new Piece();
+    // queen = new Queen();
+    // rook = new Rook();
+
     // Alocar as peças para o jogo neste construtor
 }
 
 Board::~Board()
 {
     destroyBoard();
+    // delete king;
+    // delete knight;
+    // delete pawn;
+    // delete piece;
+    // delete queen;
+    // delete rook;
     // Chamar destrutores das peças neste destrutor
 }
 
 void Board::initBoard(void)
 {
     board[8][8] =
-        't',
-    'c', 'b', 'k', 'q', 'b', 'c', 't',
+    't', 'c', 'b', 'k', 'q', 'b', 'c', 't',
     'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p',
     '0', '0', '0', '0', '0', '0', '0', '0',
     '0', '0', '0', '0', '0', '0', '0', '0',
@@ -29,8 +41,7 @@ void Board::initBoard(void)
 void Board::destroyBoard(void)
 {
     board[8][8] =
-        '0',
-    '0', '0', '0', '0', '0', '0', '0',
+    '0', '0', '0', '0', '0', '0', '0', '0',
     '0', '0', '0', '0', '0', '0', '0', '0',
     '0', '0', '0', '0', '0', '0', '0', '0',
     '0', '0', '0', '0', '0', '0', '0', '0',
@@ -40,7 +51,7 @@ void Board::destroyBoard(void)
     '0', '0', '0', '0', '0', '0', '0', '0';
 }
 
-char Board::returnBoardPosition(int x, int y)
+char Board::returnPiece(int x, int y)
 {
     return board[x][y];
 }
