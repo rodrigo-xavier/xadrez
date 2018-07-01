98c3e5085994cb610c22e24c268d6224d8d94625
Patrick Beal <patrickvrb@gmail.com>
2018-07-01 17:20:00 -0300

    Alteracao da iniciacao das pecas
    Co-authored-by: Dans1997 <danillo.neves97@gmail.com>

--- src/Initialize.cpp
+++ src/Initialize.cpp
@@ -1,4 +1,3 @@
-#include "../lib/catch.hpp"
 #include "../include/Piece.hpp"
 #include "../include/Pawn.hpp"
 #include "../include/Knight.hpp"
@@ -12,6 +11,39 @@
 Initialize::Initialize(void)
 {
 
+  Piece * white_pieces[16];
+  Piece * black_pieces[16];
+  int i;
+
+
+//Inicialização dos peões
+  for(i=0;i<=7;i++)
+  {
+    white_pieces[i] = new Pawn(true, 6, i);
+    black_pieces[i] = new Pawn(false, 1, i);
+  }
+
+  white_pieces[8]  = new Rook(true, 7, 0);
+  white_pieces[9]  = new Knight(true, 7, 1);
+  white_pieces[10] = new Bishop(true, 7, 2);
+  white_pieces[11] = new Queen(true, 7, 3);
+  white_pieces[12] = new King(true, 7, 4);
+  white_pieces[13] = new Bishop(true, 7, 5);
+  white_pieces[14] = new Knight(true, 7, 6);
+  white_pieces[15] = new Rook(true, 7, 7);
+
+  black_pieces[8]  = new Rook(false, 0, 0);
+  black_pieces[9]  = new Knight(false, 0, 1);
+  black_pieces[10] = new Bishop(false, 0, 2);
+  black_pieces[11] = new Queen(false, 0, 3);
+  black_pieces[12] = new King(false, 0, 4);
+  black_pieces[13] = new Bishop(false, 0, 5);
+  black_pieces[14] = new Knight(false, 0, 6);
+  black_pieces[15] = new Rook(false, 0, 7);
+
+  printf("\n%d\n", black_pieces[10]->GetPositionX());
+
+/*
 Rook * black_rook1 = new Rook(false, 0, 0);
 Knight * black_knight1 = new Knight(false, 0, 1);
 Bishop * black_bishop1 = new Bishop(false, 0, 2);
@@ -47,5 +79,6 @@ King * white_king = new King(true, 7, 4);
 Bishop * white_bishop2 = new Bishop(true, 7, 5);
 Knight * white_knight2 = new Knight(true, 7, 6);
 Rook * white_rook2 = new Rook(true, 7, 7);
+*/
 
 }
