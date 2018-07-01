98ce6ff4e10f5e68bb7428a1981218c1333d5f43
Patrick Beal <patrickvrb@gmail.com>
2018-07-01 17:35:53 -0300

    Aperfeicoamento do módulo Initialize
    Co-authored-by: Dans1997 <danillo.neves97@gmail.com>

--- include/Initialize.hpp
+++ include/Initialize.hpp
@@ -1,4 +1,12 @@
-class Initialize
+#include <iostream>
+#include <string>
+#include "Piece.hpp"
+
+class Initialize : public Piece
 {
+
+public:
+  using Piece::Piece;
   Initialize();
+
 };
--- src/Initialize.cpp
+++ src/Initialize.cpp
@@ -23,6 +23,8 @@ Initialize::Initialize(void)
     black_pieces[i] = new Pawn(false, 1, i);
   }
 
+
+//Inicialização das peças brancas
   white_pieces[8]  = new Rook(true, 7, 0);
   white_pieces[9]  = new Knight(true, 7, 1);
   white_pieces[10] = new Bishop(true, 7, 2);
@@ -32,6 +34,7 @@ Initialize::Initialize(void)
   white_pieces[14] = new Knight(true, 7, 6);
   white_pieces[15] = new Rook(true, 7, 7);
 
+//Inicialização das peças pretas
   black_pieces[8]  = new Rook(false, 0, 0);
   black_pieces[9]  = new Knight(false, 0, 1);
   black_pieces[10] = new Bishop(false, 0, 2);
@@ -40,45 +43,5 @@ Initialize::Initialize(void)
   black_pieces[13] = new Bishop(false, 0, 5);
   black_pieces[14] = new Knight(false, 0, 6);
   black_pieces[15] = new Rook(false, 0, 7);
-
-  printf("\n%d\n", black_pieces[10]->GetPositionX());
-
-/*
-Rook * black_rook1 = new Rook(false, 0, 0);
-Knight * black_knight1 = new Knight(false, 0, 1);
-Bishop * black_bishop1 = new Bishop(false, 0, 2);
-Queen * black_queen = new Queen(false, 0, 3);
-King * black_king = new King(false, 0, 4);
-Bishop * black_bishop2 = new Bishop(false, 0, 5);
-Knight * black_knight2 = new Knight(false, 0, 6);
-Rook * black_rook2 = new Rook(false, 0, 7);
-
-Pawn * black_pawn1 = new Pawn(false, 1, 0);
-Pawn * black_pawn2 = new Pawn(false, 1, 1);
-Pawn * black_pawn3 = new Pawn(false, 1, 2);
-Pawn * black_pawn4 = new Pawn(false, 1, 3);
-Pawn * black_pawn5 = new Pawn(false, 1, 4);
-Pawn * black_pawn6 = new Pawn(false, 1, 5);
-Pawn * black_pawn7 = new Pawn(false, 1, 6);
-Pawn * black_pawn8 = new Pawn(false, 1, 7);
-
-Pawn * white_pawn1 = new Pawn(true, 6, 0);
-Pawn * white_pawn2 = new Pawn(true, 6, 1);
-Pawn * white_pawn3 = new Pawn(true, 6, 2);
-Pawn * white_pawn4 = new Pawn(true, 6, 3);
-Pawn * white_pawn5 = new Pawn(true, 6, 4);
-Pawn * white_pawn6 = new Pawn(true, 6, 5);
-Pawn * white_pawn7 = new Pawn(true, 6, 6);
-Pawn * white_pawn8 = new Pawn(true, 6, 7);
-
-Rook * white_rook1 = new Rook(true, 7, 0);
-Knight * white_knight1 = new Knight(true, 7, 1);
-Bishop * white_bishop1 = new Bishop(true, 7, 2);
-Queen * white_queen = new Queen(true, 7, 3);
-King * white_king = new King(true, 7, 4);
-Bishop * white_bishop2 = new Bishop(true, 7, 5);
-Knight * white_knight2 = new Knight(true, 7, 6);
-Rook * white_rook2 = new Rook(true, 7, 7);
-*/
 
 }
