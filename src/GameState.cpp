32254fa0c18415bd32ea8ae33e4cf59387765cbc
pneto97 <paulo_neto@mail.com>
2018-07-08 19:20:46 -0300

    modo contra o cpu funcionando

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -160,7 +160,7 @@ void GameState::renderPVP(){
     bool showBest = false;
     int x = -1, y = -1;
     //player:
-    bool white = true;
+    //bool white = true;
 
     SDL_Event e;
     States *states = new States();
@@ -172,7 +172,7 @@ void GameState::renderPVP(){
 
     while(gameState == GameMode::GAME_MODE_PVP){
 
-            //Handle events on queue
+            //lida com a fila de eventos
             while( SDL_PollEvent( &e ) != 0 ) {
                 //User requests quit
                 if( e.type == SDL_QUIT )
@@ -210,7 +210,6 @@ void GameState::renderPVP(){
                 }
 
             }
-
 
             //limpa tela
             SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
@@ -254,44 +253,106 @@ void GameState::renderCPU(){
     bool showHint = false;
     bool showBest = false;
     int x = -1, y = -1;
-    //player:
-    bool white = true;
+
+    bool player = true; //true -> branco
+    bool CPUTurn = false; 
 
     SDL_Event e;
     States *states = new States();
     GUIBoard *tabuleiro = new GUIBoard();
     GameResult gameResult = GameResult::NoContest;
-    
-    states->SetPieceTurn(tabuleiro->choosePieceTurn(this, states));
+
+    //jogador escolhe sua peça
+    player = tabuleiro->choosePieceTurn(this,states); 
+    states->SetPieceTurn(player);
+
+    CPUTurn = false; //jogador joga primeiro
 
     while(gameState == GameMode::GAME_MODE_CPU){
-
-            //Handle events on queue
-            while( SDL_PollEvent( &e ) != 0 )
+        
+        //lida com a fila de eventos
+        while( SDL_PollEvent( &e ) != 0 ) {
+            //User requests quit
+            if( e.type == SDL_QUIT )
             {
-                //User requests quit
-                if( e.type == SDL_QUIT )
-                {
-                    gameState = GameMode::GAME_MODE_QUIT;
-                } else if(e.type == SDL_KEYDOWN){
-                    switch(e.key.keysym.sym){
-                        case SDLK_ESCAPE:
-                            renderPauseMenu();
-                            //gameState = GAME_MODE_PAUSE;
-                            break;
-                    }
+                gameState = GameMode::GAME_MODE_QUIT;
+            } else if(e.type == SDL_KEYDOWN){
+                switch(e.key.keysym.sym){
+                    case SDLK_ESCAPE:
+                        renderPauseMenu();
+                        //gameState = GAME_MODE_PAUSE;
+                        break;
+                    case SDLK_h:
+                        showHint = !showHint;
+                        break;
+                    case SDLK_b:
+                        showBest = !showBest;
                 }
+            } else if(e.type == SDL_MOUSEBUTTONDOWN){
+                switch(e.type){
+                    case SDL_MOUSEBUTTONDOWN: //se clicar
+                        if(CPUTurn == false){
+                            x = -1;
+                            y = -1;
+                            SDL_GetMouseState(&x,&y); //pega posicao do mouse
+                            tabuleiro->updateFocus((int)x,(int)y); //atualiza o indice da matriz focado
 
+                            //ve se houve jogada, atualiza os vetores de peças e zera a peça focada
+                            if(tabuleiro->checkMovement(states)){
+                                tabuleiro->focusedPiece = NULL;
+                            }else{
+                                //Se nao houve jogada, a peça focada é o foco atual
+                                tabuleiro->focusedPiece = states->GetPiece(tabuleiro->focus.x,tabuleiro->focus.y);
+                            }
+                            break;
+                            CPUTurn = true;
+                        }
+                }
             }
 
-            //Clear screen
-            SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
-            SDL_RenderClear( gRenderer );
+        }
 
-            gBoard.render(0,0);
-            //Update screen
-            SDL_RenderPresent( gRenderer );
+
+       //limpa tela
+       SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+       SDL_RenderClear( gRenderer );
+
+       //renderiza tabuleiro
+       gBoard.render(0,0);
+       
+       gameResult = states->WhoWon();
+       if(gameResult == GameResult::NoContest){
+           
+           //cpu joga
+           if(CPUTurn = true){
+               states->PlayBestMove(!player, Level::Hard);
+               CPUTurn = false;
+           }
+
+           if((tabuleiro->focusedPiece != NULL) ){
+               //foco da peça
+               if(tabuleiro->focusedPiece->GetName() != PieceName::Empty && (tabuleiro->focusedPiece->GetColor() == states->GetPieceTurn() )){
+                   pieceSelected.render(tabuleiro->indexToPixel(tabuleiro->focus.x),tabuleiro->indexToPixel(tabuleiro->focus.y)+2);
+               }
+           
+               //renderiza todos os movimentos possiveis se a peça em foco for a da jogada
+               if(showHint && (tabuleiro->focusedPiece->GetColor() == states->GetPieceTurn() ) )
+                   tabuleiro->renderPossibleMoves(states);
+
+               if(showBest &&  (tabuleiro->focusedPiece->GetColor() == states->GetPieceTurn() ))
+                   tabuleiro->renderBestMove(states);
+               //renderiza todas as peças
+               }
+           tabuleiro->renderAllPieces(states);
+
+       } else{
+           endGame[(int)gameResult].render(0,0);
+       }
+       //atualiza tela
+       SDL_RenderPresent( gRenderer );
     }
+    delete states;
+    delete tabuleiro;
 }
 
 void GameState::startStateMachine(GameMode gameMode){
